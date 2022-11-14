4638da269efc9252240e78777ed3cb53f44a7d51
LG972022 <liam.gill@method-analytics.com>
2022-11-14 14:06:04 +0000

    Added Constructor and Get Details for Bank Account Class

--- BankAccount.js
+++ BankAccount.js
@@ -1,2 +1,17 @@
-console.log("test");
-console.log("david");
+console.log("David");
+
+let bankAccount = class {
+  constructor(customerName, accountNumber, balance) {
+    this.customerName = customerName;
+    this.accountNumber = accountNumber;
+    this.balance = balance;
+  }
+
+  get Details() {
+    return [this.customerName, this.accountNumber, this.balance];
+  }
+};
+
+const BA1 = new bankAccount("David", 1234, 50000);
+
+console.log(BA1.Details);
