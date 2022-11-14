80ca4ddd265cd91d39214737a7ec6631c92c4d77
David Tsui <davidtsui_btb@davids-mbp.home>
2022-11-14 14:41:01 +0000

    exercise

--- BankAccount.js
+++ BankAccount.js
@@ -1,6 +1,7 @@
-console.log("David");
+
 
 let bankAccount = class {
+    
   constructor(customerName, accountNumber, balance) {
     this.customerName = customerName;
     this.accountNumber = accountNumber;
@@ -10,8 +11,20 @@ let bankAccount = class {
   get Details() {
     return [this.customerName, this.accountNumber, this.balance];
   }
+
+  Withdraw(amount){
+    this.balance = this.balance - amount;
+    return ("you have taken " + amount + "and you have " + this.balance + " left in the account")
+  }
+
+  Deposit(amount){
+      this.balance = this.balance + amount;
+      return ("you have deposited " + amount + "and you have " + this.balance + " left in the account")
+  }
 };
 
 const BA1 = new bankAccount("David", 1234, 50000);
 
-console.log(BA1.Details);
+
+// console.log(BA1.Details);
+console.log(BA1.Deposit(300));
